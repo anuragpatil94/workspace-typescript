b9b109e61d8e72dfb30a3be85012a443f7a5172d
Anurag Patil <aspanurag@gmail.com>
2020-01-26 17:16:50 -0500

    Step1| Error shown by TS

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,8 +2,22 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = response.data;
+  // This is basically what typescirpt is doing ->
+  // Response.data has properties of :
+  // id
+  // title
+  // completed
+
+  const todo = response.data as Todo;
+
+  // So the bug was that we wrote wrong property declarations
   const ID = todo.ID;
   const title = todo.Title;
   const finished = todo.finished;
