2f07dfc0264cc3383cc2c7d4a3e87d7e16d8638c
Anurag Patil <aspanurag@gmail.com>
2020-01-30 21:12:10 -0500

    Sort| Calling Linked List

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumberCollection";
 import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedListCollection";
 
 const numberCollection = new NumberCollection([10, 3, -5, 0]);
 const numberSorter = new Sorter(numberCollection);
@@ -8,5 +9,14 @@ numberSorter.sort();
 const characterCollection = new CharacterCollection("HelloThisIsPodo");
 const stringSorter = new Sorter(characterCollection);
 stringSorter.sort();
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(50);
+linkedList.add(-500);
+linkedList.add(530);
+linkedList.add(520);
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
 console.log(numberCollection.data);
 console.log(characterCollection.data);
+console.log(linkedList.print());
