c0aa41fe6ebe4f58384ddc1dac50f947778d20a5
Anurag Patil <aspanurag@gmail.com>
2020-02-01 12:37:51 -0500

    Initial Example

--- features/Generics.ts
+++ features/Generics.ts
@@ -47,3 +47,29 @@ const holdAnythingDate = new HoldAnything<Date>();
 // Error
 // holdAnythingDate.data = "2018/12/03";
 holdAnythingDate.data = new Date();
+
+// ------------------------------------------------------------------------------------------------------------------
+
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string) {}
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(["a", "b"]);
