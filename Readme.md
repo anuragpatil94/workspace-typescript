fe897e37518f62e8729bf12851307fd159039894
Anurag Patil <aspanurag@gmail.com>
2020-02-09 13:49:08 -0500

    Bind Route to avoid duplication

--- Readme.md
+++ Readme.md
@@ -62,6 +62,7 @@
       - [Solution on how to make sure how the decorators are ran](#solution-on-how-to-make-sure-how-the-decorators-are-ran)
       - [Metadata](#metadata)
     - [Metadata for Login](#metadata-for-login)
+    - [RouteBinding](#routebinding)
   - [Packages](#packages)
 
 ## Goals
@@ -1057,7 +1058,36 @@ export function controller(routePrefix: string) {
     }
   };
 }
+```
 
+### RouteBinding
+
+```ts
+// Avoid Duplication so create a binder to call it for different methods
+function routeBinder(method: string) {
+  return function(path: string) {
+    return (target: any, key: string, desc: PropertyDescriptor): void => {
+      /**
+       * 1st: name of the metadata variable we want to create
+       * 2nd: value of the metadata we have to store
+       * 3rd: target is the target object for which we are
+       * defining metadata in this case maybe a class.
+       * This is because as we saw that every class is stored like a object
+       * and has a prototype object.
+       * 4th: property of that object. if considered class as an object then its
+       * functions are the properties
+       */
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
+  };
+}
+
+export const get = routeBinder("get");
+export const post = routeBinder("post");
+export const put = routeBinder("put");
+export const del = routeBinder("del");
+export const patch = routeBinder("patch");
 
 ```
 
--- ts-express/src/controllers/decorators/routes.ts
+++ ts-express/src/controllers/decorators/routes.ts
@@ -1,16 +1,27 @@
 import "reflect-metadata";
-export function get(path: string) {
-  return (target: any, key: string, desc: PropertyDescriptor): void => {
-    /**
-     * 1st: name of the metadata variable we want to create
-     * 2nd: value of the metadata we have to store
-     * 3rd: target is the target object for which we are
-     * defining metadata in this case maybe a class.
-     * This is because as we saw that every class is stored like a object
-     * and has a prototype object.
-     * 4th: property of that object. if considered class as an object then its
-     * functions are the properties
-     */
-    Reflect.defineMetadata("path", path, target, key);
+
+// Avoid Duplication so create a binder to call it for different methods
+function routeBinder(method: string) {
+  return function(path: string) {
+    return (target: any, key: string, desc: PropertyDescriptor): void => {
+      /**
+       * 1st: name of the metadata variable we want to create
+       * 2nd: value of the metadata we have to store
+       * 3rd: target is the target object for which we are
+       * defining metadata in this case maybe a class.
+       * This is because as we saw that every class is stored like a object
+       * and has a prototype object.
+       * 4th: property of that object. if considered class as an object then its
+       * functions are the properties
+       */
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
   };
 }
+
+export const get = routeBinder("get");
+export const post = routeBinder("post");
+export const put = routeBinder("put");
+export const del = routeBinder("del");
+export const patch = routeBinder("patch");
