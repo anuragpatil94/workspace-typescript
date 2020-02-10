3a72c0703a32c0e225828d7e558fea717a73b335
Anurag Patil <aspanurag@gmail.com>
2020-02-10 11:56:34 -0500

    Setting up Redux| reducers

--- react-redux-ts/src/index.tsx
+++ react-redux-ts/src/index.tsx
@@ -1,43 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// All the different props we pass to the component
-interface AppProps {
-  // Making color optional.
-  color?: string;
-}
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-interface AppState {
-  counter: number;
-}
+import { reducers } from "./reducers";
 
-// Functional Component
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>;
-// };
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
+import { App } from "./components/app";
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+// // All the different props we pass to the component
+// interface AppProps {
+//   // Making color optional.
+//   color?: string;
+// }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+// interface AppState {
+//   counter: number;
+// }
 
-ReactDOM.render(<App color="red" />, document.querySelector("#root"));
+// // Functional Component
+// // const App = (props: AppProps): JSX.Element => {
+// //   return <div>{props.color}</div>;
+// // };
+
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
