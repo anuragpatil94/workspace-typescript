c54064bdb837ee8ac656335f526a5f329493ac15
Anurag Patil <aspanurag@gmail.com>
2020-02-10 13:47:03 -0500

    action creator

--- react-redux-ts/src/index.tsx
+++ react-redux-ts/src/index.tsx
@@ -6,10 +6,9 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
 import { reducers } from "./reducers";
+import { App } from "./components/app";
 
 const store = createStore(reducers, applyMiddleware(thunk));
-
-import { App } from "./components/app";
 
 // // All the different props we pass to the component
 // interface AppProps {
