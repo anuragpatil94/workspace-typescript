10806ae1854a68c0383a0bc8a19bb4070d8ca01a
Anurag Patil <aspanurag@gmail.com>
2020-02-11 09:39:49 -0500

    Action with TS using interface and generics

--- react-redux-ts/src/actions/index.ts
+++ react-redux-ts/src/actions/index.ts
@@ -1,15 +1,27 @@
 import axios from "axios";
 
 import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: "FETCH_TODOS",
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data
     });
   };
