3bfe2ec9bab56fb9fb40f4f0e82e8daf7956c04e
Anurag Patil <aspanurag@gmail.com>
2020-02-11 19:29:21 -0500

    delete reducer

--- react-redux-ts/src/actions/types.ts
+++ react-redux-ts/src/actions/types.ts
@@ -1,4 +1,7 @@
+import { FetchTodosAction, DeleteTodoAction } from "./todos";
 export enum ActionTypes {
   fetchTodos,
   deleteTodo
 }
+
+export type TodoAction = FetchTodosAction | DeleteTodoAction;
--- react-redux-ts/src/reducers/todos.ts
+++ react-redux-ts/src/reducers/todos.ts
@@ -1,9 +1,17 @@
-import { Todo, FetchTodosAction } from "../actions";
-import { ActionTypes } from "../actions/types";
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+import {
+  Todo,
+  FetchTodosAction,
+  DeleteTodoAction,
+  ActionTypes,
+  TodoAction
+} from "../actions";
+
+export const todosReducer = (state: Todo[] = [], action: TodoAction) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
