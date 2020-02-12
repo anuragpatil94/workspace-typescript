6e0ea5bdae935962f2ebad0d67685d7e7b96c256
Anurag Patil <aspanurag@gmail.com>
2020-02-11 19:38:46 -0500

    Joining Delete to App

--- react-redux-ts/src/components/app.tsx
+++ react-redux-ts/src/components/app.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Todo, fetchTodos } from "../actions";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers";
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: Function;
 }
 
 class _App extends React.Component<AppProps, StoreState> {
@@ -13,10 +14,14 @@ class _App extends React.Component<AppProps, StoreState> {
     this.props.fetchTodos();
   };
 
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
   renderList = (): JSX.Element[] => {
     return this.props.todos.map(todo => {
       return (
-        <div key={todo.id}>
+        <div key={todo.id} onClick={() => this.onTodoClick(todo.id)}>
           {todo.id} - {todo.title}
         </div>
       );
@@ -37,4 +42,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
