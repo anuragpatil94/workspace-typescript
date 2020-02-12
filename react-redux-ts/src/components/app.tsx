b80b5e28e9a064a59ec32f7e3dfc87c98ab6b0ad
Anurag Patil <aspanurag@gmail.com>
2020-02-11 19:02:41 -0500

    connect reducers to jsx

--- react-redux-ts/src/components/app.tsx
+++ react-redux-ts/src/components/app.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
 
-export class App extends React.Component{
-    render(){
-        return <div>Hi There!</div>
-    }
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
 }
+
+class _App extends React.Component<AppProps, StoreState> {
+  render() {
+    return <div>Hi There!</div>;
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps,{fetchTodos})(_App);
+
